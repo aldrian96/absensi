87a7e1c86bff2ec33833f6625b9de9087084f3d6
Moch Aldrian <mhmdsaldrian96@gmail.com>
2024-10-03 18:37:33 +0700

    Perbaikan Timezone dan Format Tanggal

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -8,15 +8,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Carbon\Carbon;
 
 class Attendance extends Model
 {
     use HasUuids;
     use HasFactory;
 
-    protected $casts = [
-        'created_at' => 'datetime:Y-m-d H:i:s',
-    ];
+    // protected $casts = [
+    //     'created_at' => 'datetime:Y-m-d H:i:s',
+    // ];
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->setTimezone(config('app.timezone'))->format('Y-m-d H:i:s');
+    }
 
     protected $fillable = [
         "user_id",
